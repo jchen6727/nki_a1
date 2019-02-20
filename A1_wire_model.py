9f6f26153b4e7205133d5a661cfa5aa38a3e392d
ericaygriffith <ericaygriffith@gmail.com>
2019-02-20 16:56:15 -0500

    Added in cell numbers according to cpernet from mpisim.py

--- A1_wire_model.py
+++ A1_wire_model.py
@@ -15,22 +15,22 @@ netParams = specs.NetParams()
 ### Cell types listed below come from labels.py -- NOT OFFICIAL YET! 2/15/19
 ### numCells should come from mpisim.py, cpernet, see Google Drive doc 
 #### Regular Spiking (RS)
-netParams.popParams['E2'] = {'cellType': 'RS', 'numCells': , 'cellModel': ''}
-netParams.popParams['E4'] = {'cellType': 'RS', 'numCells': , 'cellModel': ''}
-netParams.popParams['E5R'] = {'cellType': 'RS', 'numCells': , 'cellModel': ''}
-netParams.popParams['E6'] = {'cellType': 'RS', 'numCells': , 'cellModel': ''}
+netParams.popParams['E2'] = {'cellType': 'RS', 'numCells': 329, 'cellModel': ''}
+netParams.popParams['E4'] = {'cellType': 'RS', 'numCells': 140, 'cellModel': ''}
+netParams.popParams['E5R'] = {'cellType': 'RS', 'numCells': 18, 'cellModel': ''}
+netParams.popParams['E6'] = {'cellType': 'RS', 'numCells': 170, 'cellModel': ''}
 #### Fast Spiking (FS)
-netParams.popParams['I2'] = {'cellType': 'FS', 'numCells': , 'cellModel': ''}
-netParams.popParams['I4'] = {'cellType': 'FS', 'numCells': , 'cellModel': ''}
-netParams.popParams['I5'] = {'cellType': 'FS', 'numCells': , 'cellModel': ''}
-netParams.popParams['I6'] = {'cellType': 'FS', 'numCells': , 'cellModel': ''}
+netParams.popParams['I2'] = {'cellType': 'FS', 'numCells': 54, 'cellModel': ''}
+netParams.popParams['I4'] = {'cellType': 'FS', 'numCells': 23, 'cellModel': ''}
+netParams.popParams['I5'] = {'cellType': 'FS', 'numCells': 6, 'cellModel': ''}
+netParams.popParams['I6'] = {'cellType': 'FS', 'numCells': 28, 'cellModel': ''}
 #### Low Threshold Spiking (LTS)
-netParams.popParams['I2L'] = {'cellType': 'LTS', 'numCells': , 'cellModel': ''}
-netParams.popParams['I4L'] = {'cellType': 'LTS', 'numCells': , 'cellModel': ''}
-netParams.popParams['I5L'] = {'cellType': 'LTS', 'numCells': , 'cellModel': ''}
-netParams.popParams['I6L'] = {'cellType': 'LTS', 'numCells': , 'cellModel': ''}
+netParams.popParams['I2L'] = {'cellType': 'LTS', 'numCells': 54, 'cellModel': ''}
+netParams.popParams['I4L'] = {'cellType': 'LTS', 'numCells': 23, 'cellModel': ''}
+netParams.popParams['I5L'] = {'cellType': 'LTS', 'numCells': 6, 'cellModel': ''}
+netParams.popParams['I6L'] = {'cellType': 'LTS', 'numCells': 28, 'cellModel': ''}
 #### Burst Firing (Burst)
-netParams.popParams['E5B'] = {'cellType': 'Burst', 'numCells': , 'cellModel': ''}
+netParams.popParams['E5B'] = {'cellType': 'Burst', 'numCells': 26, 'cellModel': ''}
 ### THALAMUS (core)-- excluded for now per samn (2/17/19)
 # netParams.popParams['TC'] = {'cellType': 'thal_core', 'numCells': 0, 'cellModel': ''}
 # netParams.popParams['HTC'] = {'cellType': 'thal_core', 'numCells': 0, 'cellModel': ''}
