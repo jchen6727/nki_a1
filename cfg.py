147a648c63797eab00c1da862dfdd00953323dcb
ericagriffith <ericaygriffith@gmail.com>
2019-03-03 17:49:17 -0600

    set up more of the basic config infrastructure

--- cfg.py
+++ cfg.py
@@ -23,17 +23,19 @@ cfg = specs.SimConfig()
 # Run parameters
 #------------------------------------------------------------------------------
 
-
-
-
+cfg.duration = 0.5*1e3			## Duration of the sim, in ms -- value from M1 cfg.py 
+cfg.dt = 0.05                   ## Internal Integration Time Step -- value from M1 cfg.py 
+cfg.verbose = False           	## Show detailed messages
 
 
 #------------------------------------------------------------------------------
 # Recording 
 #------------------------------------------------------------------------------
 
-
-
+cfg.recordTraces = {}         	## Dict with traces to record
+cfg.recordStim = False			## Seen in M1 cfg.py
+cfg.recordTime = False  		## SEen in M1 cfg.py 
+cfg.recordStep = 0.1            ## Step size (in ms) to save data -- value from M1 cfg.py 
 
 
 
@@ -41,16 +43,22 @@ cfg = specs.SimConfig()
 # Saving
 #------------------------------------------------------------------------------
 
-
-
-
+cfg.filename =                	## Set file output name
+cfg.savePickle = True         	## Save pkl file
+cfg.saveJson = True           	## Save json file
+cfg.saveDataInclude = ['simData', 'simConfig', 'netParams'] ## seen in M1 cfg.py (line 58)
+cfg.backupCfgFile = None 		## Seen in M1 cfg.py 
+cfg.gatherOnlySimData = False	## Seen in M1 cfg.py 
+cfg.saveCellSecs = True			## Seen in M1 cfg.py 
+cfg.saveCellConns = True		## Seen in M1 cfg.py 
 
 #------------------------------------------------------------------------------
 # Analysis and plotting 
 #------------------------------------------------------------------------------
 
-
-
+cfg.analysis['plotTraces'] = {} 		## Seen in M1 cfg.py (line 68) 
+cfg.analysis['plotRaster'] = True     	## Plot a raster
+cfg.analysis['plot2Dnet'] = True      	## Plot 2D visualization of cell positions & connections 
 
 #------------------------------------------------------------------------------
 # Cells
@@ -72,3 +80,12 @@ cfg = specs.SimConfig()
 #------------------------------------------------------------------------------
 # Network 
 #------------------------------------------------------------------------------
+
+## These values taken from M1 cfg.py (https://github.com/Neurosim-lab/netpyne/blob/development/examples/M1detailed/cfg.py)
+cfg.scale = #1.0
+cfg.sizeY = #1350.0
+cfg.sizeX = #400.0
+cfg.sizeZ = #400.0
+cfg.scaleDensity = #0.01
+
+
