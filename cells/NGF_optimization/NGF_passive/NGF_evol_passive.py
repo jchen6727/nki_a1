097c374df540ca2faf42a4b3faca231b030e4196
salvadord <salvadordura@gmail.com>
2019-08-10 11:24:38 -0400

    added a cands threhold

--- cells/NGF_optimization/NGF_passive/NGF_evol_passive.py
+++ cells/NGF_optimization/NGF_passive/NGF_evol_passive.py
@@ -33,142 +33,142 @@ def generate_netparams(random, args):
 ###### FITNESS FUNCTION ######
 # Design fitness function, used in the ec evolve function --> final_pop = my_ec.evolve(...,evaluator=evaluate_netparams,...)
 def evaluate_netparams(candidates, args):
-	global ngen   # This will keep track of the number of generations 
-	ngen +=1
+    global ngen   # This will keep track of the number of generations 
+    ngen +=1
 
 ############################################################################################################
 
-	###### SUBMIT BATCH JOBS FOR ALL CANDIDATES IN THE CURRENT GENERATION ######
+    ###### SUBMIT BATCH JOBS FOR ALL CANDIDATES IN THE CURRENT GENERATION ######
 
 ############################################################################################################
 
-	print('BATCH SUBMISSIONS FOR GEN ' + str(ngen) + ' ARE BEGINNING')
+    print('BATCH SUBMISSIONS FOR GEN ' + str(ngen) + ' ARE BEGINNING')
 
-	for icand,cand in enumerate(candidates): 
-		# Modify network params in NGF_netParams.py based on this candidate's params ("genes")
+    for icand,cand in enumerate(candidates): 
+        # Modify network params in NGF_netParams.py based on this candidate's params ("genes")
 
 ############## CHANGE THESE BASED ON WHAT NEEDS TO BE MODIFIED ###########
 
-		## read the lines in the file
-		fo = open('NGF_netParams.py', 'r')
-		lines = fo.readlines()
-		fo.close()
-		## overwrite old lines
-		fo = open('NGF_netParams.py', 'w')
-		for line in lines: # CHANGE THIS TO MATCH NUMBER OF MODIFIED PARAMS
-			if line != lines[len(lines)-1] and line != lines[len(lines)-2] and line != lines[len(lines)-3]: # requires that there be the desired lines already at the end
-				fo.write(line)
-		fo.close()
+        ## read the lines in the file
+        fo = open('NGF_netParams.py', 'r')
+        lines = fo.readlines()
+        fo.close()
+        ## overwrite old lines
+        fo = open('NGF_netParams.py', 'w')
+        for line in lines: # CHANGE THIS TO MATCH NUMBER OF MODIFIED PARAMS
+            if line != lines[len(lines)-1] and line != lines[len(lines)-2] and line != lines[len(lines)-3]: # requires that there be the desired lines already at the end
+                fo.write(line)
+        fo.close()
 
-		## append new lines  
-		with open('NGF_netParams.py', 'a') as fo: # CHANGE THIS TO REFLECT THE MECHANISMS BEING CHANGED
-			fo.write("cellRule['NGF_Rule']['secs']['soma']['mechs']['ch_Navngf']['gmax'] = " + str(cand[0]) + "\ncellRule['NGF_Rule']['secs']['soma']['mechs']['ch_Navngf']['ena'] = " + str(cand[1]) + "\ncellRule['NGF_Rule']['secs']['soma']['geom']['cm'] =  " + str(cand[2]))
+        ## append new lines  
+        with open('NGF_netParams.py', 'a') as fo: # CHANGE THIS TO REFLECT THE MECHANISMS BEING CHANGED
+            fo.write("cellRule['NGF_Rule']['secs']['soma']['mechs']['ch_Navngf']['gmax'] = " + str(cand[0]) + "\ncellRule['NGF_Rule']['secs']['soma']['mechs']['ch_Navngf']['ena'] = " + str(cand[1]) + "\ncellRule['NGF_Rule']['secs']['soma']['geom']['cm'] =  " + str(cand[2]))
 
-		# Run batch using the above candidate params
-		NGF_batch_passive.batch_full(icand, ngen, runType)
-		print('BATCH JOB FOR CAND_' + str(icand) + ' FROM GEN_' + str(ngen) + ' SUBMITTED')
+        # Run batch using the above candidate params
+        NGF_batch_passive.batch_full(icand, ngen, runType)
+        print('BATCH JOB FOR CAND_' + str(icand) + ' FROM GEN_' + str(ngen) + ' SUBMITTED')
 
-	print('END OF BATCH SUBMISSIONS FOR GEN ' + str(ngen))
+    print('END OF BATCH SUBMISSIONS FOR GEN ' + str(ngen))
 
 
 ############################################################################################################
 
-	###### CALCULATE FITNESS AS BATCH RUNS COMPLETE ######
+    ###### CALCULATE FITNESS AS BATCH RUNS COMPLETE ######
 
 ############################################################################################################
 
 #### CHANGE FITNESS FUNCTIONS AS NEEDED #### 
 
-	global data_path_stem
-	data_path = data_path_stem + str(ngen) + '/'
+    global data_path_stem
+    data_path = data_path_stem + str(ngen) + '/'
 
-	cands_completed = 0
-	total_cands = len(candidates)
-	fitnessCandidates = [None for cand in candidates]
+    cands_completed = 0
+    total_cands = len(candidates)
+    fitnessCandidates = [None for cand in candidates]
 
+    cands_threshold = 0.9
+    while cands_completed < int(cands_threshold*total_cands):
+        print(str(cands_completed) + ' / ' + str(total_cands) + ' candidates completed')
 
-	while cands_completed < total_cands:
-		print(str(cands_completed) + ' / ' + str(total_cands) + ' candidates completed')
+        unfinished = [i for i, x in enumerate(fitnessCandidates) if x is None]
 
-		unfinished = [i for i, x in enumerate(fitnessCandidates) if x is None]
+        for cand_index in unfinished:
+            data_files = os.listdir(data_path) # EXISTING DATA FILES
+            #print('data_path = ' + data_path) #CHECK STATEMENT
 
-		for cand_index in unfinished:
-			data_files = os.listdir(data_path) # EXISTING DATA FILES
-			#print('data_path = ' + data_path) #CHECK STATEMENT
-
-			temp = []
-			for i in data_files:
-				if '.json' in i:
-					temp.append(i)
-			
-			data_files_DONE = []
-			for i in temp:
-				if 'batch.json' not in i and 'cfg.json' not in i:
-					data_files_DONE.append(i)
-
-
-			data_files_DONE.sort()
-			#print('data_files_DONE is below')#CHECK STATEMENT
-			#print(data_files_DONE)
-
-			data_file_root = 'NGF_batch_data_cand' + str(cand_index) 
-			#print('data_file_root =' + data_file_root)
-			data_files_NEEDED = [data_file_root + '_0.json', data_file_root + '_1.json', data_file_root + '_2.json', data_file_root + '_3.json', data_file_root + '_4.json', data_file_root + '_5.json', data_file_root + '_6.json', data_file_root + '_7.json', data_file_root + '_8.json', data_file_root + '_9.json', data_file_root + '_10.json', data_file_root + '_11.json', data_file_root + '_12.json']
-			data_files_NEEDED.sort()
-
-			#print('data_files_NEEDED is below')#CHECK STATEMENT
-			#print(data_files_NEEDED)
-
-			if set(data_files_NEEDED).issubset(set(data_files_DONE)):
-				print('BATCH RUN FOR CAND ' + str(cand_index) + ' IS COMPLETE')
-				print('FITNESS CALCULATIONS FOR CAND ' + str(cand_index) + ' BEGINNING')
-
-			############ FITNESS CALCULATIONS ##########################
-				###### EXTRACT THE MEAN STEADY STATE VOLTAGE RESPONSES ###### 
-				# RMP_ritness = eval_RMP_fitness(ARGS)
-				mean_responses = [None for data in data_files_NEEDED]
-				for i in range(len(data_files_NEEDED)):
-					outputData = json.load(open(data_path + data_file_root + '_' + str(i) + '.json'))
-					vdata_total = list(outputData['simData']['V_soma']['cell_0'])
-					vdata = vdata_total[2000:] # accounts for delay 
-					steady_state_response = mean(vdata)
-					mean_responses[i] = steady_state_response
-
-				###### RMP ######
-				RMP_sim = mean_responses[0]
-				RMP_fitness = abs(RMP_sim-RMP)**2 # RMP defined in main code
-				outputData_RMP = json.load(open(data_path + data_file_root + '_0.json'))
+            temp = []
+            for i in data_files:
+                if '.json' in i:
+                    temp.append(i)
+            
+            data_files_DONE = []
+            for i in temp:
+                if 'batch.json' not in i and 'cfg.json' not in i:
+                    data_files_DONE.append(i)
 
 
-				###### SUBTHRESHOLD RESPONSES #####
-				# subthresh_fitness = eval_subthresh_fitness(ARGS)
-				sim_responses = mean_responses[1:]
-				sim_deltas = [None for n in sim_responses]
-				for i in range(len(sim_responses)):
-					sim_deltas[i] = sim_responses[i] - RMP_sim
+            data_files_DONE.sort()
+            #print('data_files_DONE is below')#CHECK STATEMENT
+            #print(data_files_DONE)
 
-				subthresh_comparison = [None for x in subthreshold_deltas]
-				for i in range(len(sim_deltas)):
-					subthresh_comparison[i] = (sim_deltas[i] - subthreshold_deltas[i])**2
+            data_file_root = 'NGF_batch_data_cand' + str(cand_index) 
+            #print('data_file_root =' + data_file_root)
+            data_files_NEEDED = [data_file_root + '_0.json', data_file_root + '_1.json', data_file_root + '_2.json', data_file_root + '_3.json', data_file_root + '_4.json', data_file_root + '_5.json', data_file_root + '_6.json', data_file_root + '_7.json', data_file_root + '_8.json', data_file_root + '_9.json', data_file_root + '_10.json', data_file_root + '_11.json', data_file_root + '_12.json']
+            data_files_NEEDED.sort()
 
-				subthresh_fitness = 0
-				for i in range(len(subthresh_comparison)):
-					subthresh_fitness += subthresh_comparison[i]
+            #print('data_files_NEEDED is below')#CHECK STATEMENT
+            #print(data_files_NEEDED)
 
-				### POPULATE FITNESS OBJECT 
-				fitnessCandidates[cand_index] = ec.emo.Pareto([RMP_fitness, subthresh_fitness], maximize = [False, False])
+            if set(data_files_NEEDED).issubset(set(data_files_DONE)):
+                print('BATCH RUN FOR CAND ' + str(cand_index) + ' IS COMPLETE')
+                print('FITNESS CALCULATIONS FOR CAND ' + str(cand_index) + ' BEGINNING')
 
-				cands_completed += 1
-				print('FITNESS EVALUATIONS FOR CANDIDATE ' + str(cand_index) + ' ARE COMPLETE')
+            ############ FITNESS CALCULATIONS ##########################
+                ###### EXTRACT THE MEAN STEADY STATE VOLTAGE RESPONSES ###### 
+                # RMP_ritness = eval_RMP_fitness(ARGS)
+                mean_responses = [None for data in data_files_NEEDED]
+                for i in range(len(data_files_NEEDED)):
+                    outputData = json.load(open(data_path + data_file_root + '_' + str(i) + '.json'))
+                    vdata_total = list(outputData['simData']['V_soma']['cell_0'])
+                    vdata = vdata_total[2000:] # accounts for delay 
+                    steady_state_response = mean(vdata)
+                    mean_responses[i] = steady_state_response
 
-			else:
-				sleep(0.1)
-				print('Candidate ' + str(cand_index) + ' is unfinished')
+                ###### RMP ######
+                RMP_sim = mean_responses[0]
+                RMP_fitness = abs(RMP_sim-RMP)**2 # RMP defined in main code
+                outputData_RMP = json.load(open(data_path + data_file_root + '_0.json'))
 
 
-	print('EVALUATION OF GENERATION ' + str(ngen) + ' IS COMPLETE')
-	print(fitnessCandidates)
-	return fitnessCandidates
+                ###### SUBTHRESHOLD RESPONSES #####
+                # subthresh_fitness = eval_subthresh_fitness(ARGS)
+                sim_responses = mean_responses[1:]
+                sim_deltas = [None for n in sim_responses]
+                for i in range(len(sim_responses)):
+                    sim_deltas[i] = sim_responses[i] - RMP_sim
+
+                subthresh_comparison = [None for x in subthreshold_deltas]
+                for i in range(len(sim_deltas)):
+                    subthresh_comparison[i] = (sim_deltas[i] - subthreshold_deltas[i])**2
+
+                subthresh_fitness = 0
+                for i in range(len(subthresh_comparison)):
+                    subthresh_fitness += subthresh_comparison[i]
+
+                ### POPULATE FITNESS OBJECT 
+                fitnessCandidates[cand_index] = ec.emo.Pareto([RMP_fitness, subthresh_fitness], maximize = [False, False])
+
+                cands_completed += 1
+                print('FITNESS EVALUATIONS FOR CANDIDATE ' + str(cand_index) + ' ARE COMPLETE')
+
+            else:
+                sleep(0.1)
+                print('Candidate ' + str(cand_index) + ' is unfinished')
+
+
+    print('EVALUATION OF GENERATION ' + str(ngen) + ' IS COMPLETE')
+    print(fitnessCandidates)
+    return fitnessCandidates
 
 
 # Generation tracking
@@ -179,19 +179,19 @@ ngen = -1
 machine_ID = input('COMET or GCP or LOCAL or zn?')
 
 if machine_ID == 'COMET':
-	data_path_stem = '/oasis/scratch/comet/eyg42/temp_project/A1/NGF_passive/NGF_batch_data_gen_'
-	runType = 'hpc_slurm'
+    data_path_stem = '/oasis/scratch/comet/eyg42/temp_project/A1/NGF_passive/NGF_batch_data_gen_'
+    runType = 'hpc_slurm'
 elif machine_ID == 'GCP':
-	data_path_stem = '/home/salvadord/A1/cells/NGF_optimization/NGF_passive/NGF_batch_data_gen_'
-	runType = 'hpc_slurm_gcp'
+    data_path_stem = '/home/salvadord/A1/cells/NGF_optimization/NGF_passive/NGF_batch_data_gen_'
+    runType = 'hpc_slurm_gcp'
 elif machine_ID == 'LOCAL':
-	data_path_stem = '/Users/ericagriffith/Desktop/NEUROSIM/A1/cells/NGF_optimization/NGF_passive/NGF_batch_data_gen_'
-	runType = 'mpi_bulletin'
+    data_path_stem = '/Users/ericagriffith/Desktop/NEUROSIM/A1/cells/NGF_optimization/NGF_passive/NGF_batch_data_gen_'
+    runType = 'mpi_bulletin'
 elif machine_ID == 'zn':
-	data_path_stem = '/u/ericag/A1/cells/NGF_optimization/NGF_passive/NGF_batch_data_gen_'
-	runType = 'mpi_bulletin'
+    data_path_stem = '/u/ericag/A1/cells/NGF_optimization/NGF_passive/NGF_batch_data_gen_'
+    runType = 'mpi_bulletin'
 else:
-	raise Exception("Computing system not recognized")
+    raise Exception("Computing system not recognized")
 
 
 # create random seed for evolutionary computation algorithm --> my_ec = ec.EvolutionaryComputation(rand)
