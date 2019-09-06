0054a6c29c3785307ad7b8811a1fe0165a08f723
ericagriffith <ericaygriffith@gmail.com>
2019-09-06 16:08:00 -0400

    removing NGF_optimization files, retaining a copy locally in separate location if needed

--- cells/NGF_optimization/NGF_init.py
+++
@@ -1,5 +0,0 @@
-from netpyne import sim
-# read cfg and netParams from command line arguments if available; otherwise use default
-simConfig,netParams=sim.readCmdLineArgs(simConfigDefault='NGF_cfg.py',netParamsDefault='NGF_netParams.py')
-# Create network and run simulation
-sim.createSimulateAnalyze(netParams=netParams,simConfig=simConfig)
