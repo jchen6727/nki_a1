3e0272f4ffafed43d9508aeab5925290bea612bf
salvadord <salvadordura@gmail.com>
2019-10-04 16:31:01 -0400

    updating E-E and E-I conn based on Allen

--- conn/conn.py
+++ conn/conn.py
@@ -7,15 +7,82 @@ Local connectivity preprocessing script
 
 '''
 import numpy as np
+import json
+import csv
 
-# Load data from published studies
+# ----------------------------------------------------------------------------------------------------------------
+# Func to load data from published studies
+# ----------------------------------------------------------------------------------------------------------------
 def loadData():
-    data = {'prob': 0.2, 'weight': 0.5}  # oversimplified example -- will have data from different papers for differnet projections 
+    data = {'test': {'prob': 0.2, 'weight': 0.5}}  # oversimplified example -- will have data from different papers for differnet projections 
+    
+    # ----------------------------------------------------------------------------------------------------------------
+    # load and pre-process Allen mouse V1 data (Billeh et al, 2019; https://www.dropbox.com/sh/xb7xasih3d8027u/AAAbKXe0Zmk86o3_y1iPVPCLa?dl=0)
+    data['Allen_V1'] = {}
+    
+    ## load conn probs
+    with open('../data/conn/Allen_V1_conn_probs.json', 'r') as f:
+        data['Allen_V1']['connProb'] = json.load(f)
+
+    ## func to calculate probConn at distance = 0um based on probConn at distance = 75 and lambda (length constant)
+    ## adapted from Allen V1 code in Allen_V1_connect_cells.py
+
+    for proj in data['Allen_V1']['connProb'].keys():
+
+        # A_literature is different for every source-target pair and was estimated from the literature.
+        A_literature = data['Allen_V1']['connProb'][proj]['A_literature']
+
+        # R0 read from the dictionary, but setting it now at 75um for all cases but this allows us to change it
+        R0 = data['Allen_V1']['connProb'][proj]['R0']
+
+        # Sigma is measure from the literature or internally at the Allen Institute
+        sigma = data['Allen_V1']['connProb'][proj]['sigma']
+
+        # Gaussian equation was intergrated to and solved to calculate new A0 (amplitude at distance=0)
+        A0 = A_literature / ((sigma / R0)** 2 * (1 - np.exp(-(R0 / sigma)** 2)))
+        if A0 > 1.0: A0 = 1.0  # make max prob = 1.0
+        data['Allen_V1']['connProb'][proj]['A0'] = A0
+
+
+    ## load conn weights (soma PSP amp in mV)
+    data['Allen_V1']['connWeight'] = {}
+    with open('../data/conn/Allen_V1_conn_weights.csv', 'r') as f:
+        csv_reader = csv.reader(f)
+        for irow,row in enumerate(csv_reader):
+            if irow == 0:
+                headings = row
+            else:
+                for ih, h in enumerate(headings[1:]):
+                    data['Allen_V1']['connWeight'][row[0] + '-' + h] = row[ih]
+                
+    
+    # set correspondence between A1 pops and Allen V1 pops 
+    data['Allen_V1']['pops'] = {'IT2': 'e2', 'IT3': 'e2', 'ITP4': 'e4', 'ITS4': 'e4', 'IT5A': 'e5', 'IT5B': 'e5', 'PT5B': 'e5', 'IT6': 'e6', 'CT6': 'e6'}  # all layers
+    data['Allen_V1']['pops'] = {'NGF1': 'i1H',                            # L1
+                                'PV2': 'i23P', 'SOM2': 'i23S', 'VIP2': 'i23H', 'NGF2': 'i23H',  # L2/3
+                                'PV3': 'i3P', 'SOM3': 'i3S', 'VIP3': 'i3H', 'NGF3': 'i3H',      # L3
+                                'PV4': 'i4P', 'SOM4': 'i4S', 'VIP4': 'i4H', 'NGF4': 'i4H',      # L4
+                                'PV5A': 'i5P', 'SOM5A': 'i5S', 'VIP5A': 'i5H', 'NGF5A': 'i5H',      # L5A
+                                'PV5B': 'i5P', 'SOM5B': 'i5S', 'VIP5B': 'i5H', 'NGF5B': 'i5H',      # L5B
+                                'PV6': 'i6P', 'SOM6': 'i6S', 'VIP6': 'i6H', 'NGF6': 'i6H'}      # L6
+
+
+    # ----------------------------------------------------------------------------------------------------------------
+    # load and pre-process BBP mouse S1 data (Markram et al, 2015; https://bbp.epfl.ch/nmc-portal/downloads
+    data['BBP_S1'] = {}
+    with open('../data/conn/BBP_S1_pathways_anatomy_factsheets_simplified.json', 'r') as f:
+        data['BBP_S1']['connProb'] = json.load(f)
+
+    with open('../data/conn/BBP_S1_pathways_physiology_factsheets_simplified.json', 'r') as f:
+        data['BBP_S1']['connWeight'] = json.load(f)
+    
     return data
 
+# ----------------------------------------------------------------------------------------------------------------
 # Params
+# ----------------------------------------------------------------------------------------------------------------
 Etypes = ['IT', 'ITS4', 'PT', 'CT']
-Epops = ['IT2', 'IT3', 'ITP4', 'ITS4', 'IT5A', 'IT5B', 'PT5B', 'IT6', 'CT6']  # all layers
+Epops = ['IT2', 'IT3', 'ITP4', 'ITS4', 'IT6', 'IT5B', 'PT5B', 'IT6', 'CT6']  # all layers
 
 Itypes = ['PV', 'SOM', 'VIP', 'NGF']
 Ipops = ['NGF1',                            # L1
@@ -28,19 +95,20 @@ Ipops = ['NGF1',                            # L1
         
 layer = {'1': [0.00, 0.05], '2': [0.05, 0.08], '3': [0.08, 0.475], '4': [0.475,0.625], '5A': [0.625,0.667], '5B': [0.667,0.775], '6': [0.775,1]} # 
 
+
+
 # initialize prob and weight matrices
 # format: pmat[presynaptic_pop][postsynaptic_pop] 
-pmat = {}
-for p in Epops+Ipops: pmat[p] = {}  
-
-wmat = {}
-for p in Epops+Ipops: wmat[p] = {}
-
+pmat = {}  # probability of connection matrix
+lmat = {}  # length constant (lambda) for exp decaying prob conn (um) matrix
+wmat = {}  # connection weight matrix = unitary conn somatic PSP (mV)
+for p in Epops + Ipops:
+    pmat[p] = {}
+    lmat[p] = {}
+    wmat[p] = {}
+    
 # Load exp data
 data = loadData()
-
-# Note: below shows just a simplified example of how the pmat and wmat dicts can be filled in.
-# When using real exp it wil be much more complicated, require dividing by groups of pops, exceptions etc 
 
 
 # --------------------------------------------------
@@ -48,30 +116,28 @@ data = loadData()
 # --------------------------------------------------
 
 # --------------------------------------------------
-## Probabilities 
-for pop in Epops:
-    pmat['IT2'][pop] = data['prob']  
-    pmat['IT3'][pop] = data['prob']  
-    pmat['ITP4'][pop] = data['prob']  
-    pmat['ITS4'][pop] = data['prob']  
-    pmat['IT5A'][pop] = data['prob']  
-    pmat['IT5B'][pop] = data['prob']  
-    pmat['PT5B'][pop] = data['prob']  
-    pmat['IT6'][pop] = data['prob']  
-    pmat['CT6'][pop] = data['prob']  
+## Probabilities, length constants (lambda), and weights (=unitary conn somatic PSP amplitude)
 
-# --------------------------------------------------
-## Weights  (=unitary conn somatic PSP amplitude)
-for pop in Epops:
-    wmat['IT2'][pop] = data['weight']  
-    wmat['IT3'][pop] = data['weight']  
-    wmat['ITP4'][pop] = data['weight']  
-    wmat['ITS4'][pop] = data['weight']  
-    wmat['IT5A'][pop] = data['weight']  
-    wmat['IT5B'][pop] = data['weight']  
-    wmat['PT5B'][pop] = data['weight']  
-    wmat['IT6'][pop] = data['weight']  
-    wmat['CT6'][pop] = data['weight']  
+'''
+Probabilities are distance dependent based on:
+pconn0 * np.exp(- (intersomatic_distance / lambda) ** 2)
+
+where pconn0 is the probability of connection and distance 0um
+and lambda is the length constant
+'''
+
+# start with base data from Allen V1
+for pre in Epops:
+    for post in Epops:
+        proj = '%s-%s' % (data['Allen_V1']['pops'][pre], data['Allen_V1']['pops'][post])
+        pmat[pre][post] = data['Allen_V1']['connProb'][proj]['A0']
+        lmat[pre][post] = data['Allen_V1']['connProb'][proj]['sigma']
+        wmat[pre][post] = data['Allen_V1']['connWeight']['A0'][proj]
+
+# use BBP S1 instead? (has more cell-type specificity)
+
+# modify based on A1 exp data - TO DO
+
 
 # --------------------------------------------------
 ## E -> I
@@ -80,29 +146,29 @@ for pop in Epops:
 # --------------------------------------------------
 ## Probabilities 
 for pop in Ipops:
-    pmat['IT2'][pop] = data['prob']  
-    pmat['IT3'][pop] = data['prob']  
-    pmat['ITP4'][pop] = data['prob']  
-    pmat['ITS4'][pop] = data['prob']  
-    pmat['IT5A'][pop] = data['prob']  
-    pmat['IT5B'][pop] = data['prob']  
-    pmat['PT5B'][pop] = data['prob']  
-    pmat['IT6'][pop] = data['prob']  
-    pmat['CT6'][pop] = data['prob']  
+    pmat['IT2'][pop] = data['test']['prob']  
+    pmat['IT3'][pop] = data['test']['prob']  
+    pmat['ITP4'][pop] = data['test']['prob']  
+    pmat['ITS4'][pop] = data['test']['prob']  
+    pmat['IT5A'][pop] = data['test']['prob']  
+    pmat['IT5B'][pop] = data['test']['prob']  
+    pmat['PT5B'][pop] = data['test']['prob']  
+    pmat['IT6'][pop] = data['test']['prob']  
+    pmat['CT6'][pop] = data['test']['prob']  
 
 
 # --------------------------------------------------
 ## Weights  (=unitary conn somatic PSP amplitude)
 for pop in Ipops:
-    wmat['IT2'][pop] = data['weight']  
-    wmat['IT3'][pop] = data['weight']  
-    wmat['ITP4'][pop] = data['weight']  
-    wmat['ITS4'][pop] = data['weight']  
-    wmat['IT5A'][pop] = data['weight']  
-    wmat['IT5B'][pop] = data['weight']  
-    wmat['PT5B'][pop] = data['weight']  
-    wmat['IT6'][pop] = data['weight']  
-    wmat['CT6'][pop] = data['weight']
+    wmat['IT2'][pop] = data['test']['weight']  
+    wmat['IT3'][pop] = data['test']['weight']  
+    wmat['ITP4'][pop] = data['test']['weight']  
+    wmat['ITS4'][pop] = data['test']['weight']  
+    wmat['IT5A'][pop] = data['test']['weight']  
+    wmat['IT5B'][pop] = data['test']['weight']  
+    wmat['PT5B'][pop] = data['test']['weight']  
+    wmat['IT6'][pop] = data['test']['weight']  
+    wmat['CT6'][pop] = data['test']['weight']
     
 
 # --------------------------------------------------
