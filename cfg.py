19e84af829e879a441ea20c4f8e75b67faca22c3
salvadord <salvadordura@gmail.com>
2019-10-17 17:20:04 -0400

    v11_sim14

--- cfg.py
+++ cfg.py
@@ -45,7 +45,7 @@ cfg.recordStep = 0.1            ## Step size (in ms) to save data -- value from 
 # Saving
 #------------------------------------------------------------------------------
 
-cfg.simLabel = 'v11_sim13'
+cfg.simLabel = 'v11_sim14'
 cfg.saveFolder = 'data/v11_manualTune/'                	## Set file output name
 cfg.savePickle = True         	## Save pkl file
 cfg.saveJson = True           	## Save json file
@@ -59,7 +59,7 @@ cfg.saveCellConns = False		## Seen in M1 cfg.py
 # Analysis and plotting 
 #------------------------------------------------------------------------------
 
-cfg.analysis['plotTraces'] = {'include': [0,100, 1285, 1787,2422, 2464, 2470,30,97,98], 'overlay': True, 'saveFig': True, 'showFig': False} #[(pop,0) for pop in alltypes]		## Seen in M1 cfg.py (line 68) 
+cfg.analysis['plotTraces'] = {'include': [0,100, 1285, 1787,2422, 2464, 2470,30,97,98], 'oneFigPer': 'trace', 'overlay': True, 'saveFig': True, 'showFig': False} #[(pop,0) for pop in alltypes]		## Seen in M1 cfg.py (line 68) 
 cfg.analysis['plotRaster'] = {'include': allpops, 'saveFig': True, 'showFig': False, 'popRates': True, 'orderInverse': True, 'timeRange': [0,cfg.duration], 'figSize': (12,12), 'lw': 0.3, 'markerSize':10, 'marker': '.', 'dpi': 300}      	## Plot a raster
 #cfg.analysis['plotLFP'] = {'include': ['timeSeries', 'PSD', 'spectrogram']}
 #cfg.analysis['plot2Dnet'] = True      	## Plot 2D visualization of cell positions & connections 
@@ -101,9 +101,9 @@ cfg.synWeightFractionII = [0.9, 0.1]  # SOM -> E GABAASlow to GABAB ratio (updat
 
 # Cortical
 cfg.addConn = 1
-cfg.EEGain = 1.0 
+cfg.EEGain = 3.0 
 cfg.EIGain = 0.5
-cfg.IEGain = 0.5
+cfg.IEGain = 2.0
 cfg.IIGain = 0.5
 
 ## I->E/I layer weights (L2/3+4, L5, L6)
@@ -128,10 +128,10 @@ cfg.matrixThalamoCorticalGain = 1.0
 # Background inputs
 #------------------------------------------------------------------------------
 cfg.addBkgConn = 1
-cfg.noiseBkg = 1.0  # firing rate random noise
+cfg.noiseBkg = {'A1': 1.0, 'thalamus': 1.0}  # firing rate random noise
 cfg.delayBkg = 5.0  # (ms)
 cfg.startBkg = 0  # start at 0 ms
-cfg.weightBkg = {'E': 3.0, 'I': 2.0, 'ThalE': 3.0, 'ThalI': 2.0}  # corresponds to unitary connection somatic EPSP (mV)
+cfg.weightBkg = {'E': 3.0, 'I': 2.0, 'ThalE': 4.0, 'ThalI': 2.0}  # corresponds to unitary connection somatic EPSP (mV)
 cfg.rateBkg = {'E': 80, 'I': 80, 'ThalE': 80, 'ThalI': 80}
 
 
