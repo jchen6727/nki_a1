9231d7b45721968e3892f3920b4be560ddffd360
salvadord <salvadordura@gmail.com>
2020-02-18 17:35:32 -0500

    testing different network sizes

--- init.py
+++ init.py
@@ -21,13 +21,13 @@ from netpyne import sim
 
 cfg, netParams = sim.readCmdLineArgs(simConfigDefault='cfg.py', netParamsDefault='netParams.py')
 
-# sim.createSimulateAnalyze(netParams, cfg)
+sim.createSimulateAnalyze(netParams, cfg)
 
-sim.initialize(
-    simConfig = cfg, 	
-    netParams = netParams)  				# create network object and set cfg and net params
-sim.net.createPops()               			# instantiate network populations
-sim.net.createCells()              			# instantiate network cells based on defined populations
+# sim.initialize(
+#     simConfig = cfg, 	
+#     netParams = netParams)  				# create network object and set cfg and net params
+# sim.net.createPops()               			# instantiate network populations
+# sim.net.createCells()              			# instantiate network cells based on defined populations
 # sim.net.connectCells()            			# create connections between cells based on params
 # sim.net.addStims() 							# add network stimulation
 # sim.setupRecording()              			# setup variables to record for each cell (spikes, V traces, etc)
