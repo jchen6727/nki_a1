c3c3605355f9ac25fa69250c448b4e955339a5c4
ericagriffith <ericaygriffith@gmail.com>
2020-02-28 14:33:08 -0500

    changed mod file suffixes to avoid conflict with mechanisms in other cell types

--- cells/sTI.py
+++ cells/sTI.py
@@ -81,13 +81,13 @@ cellRule['secs']['soma']['mechs']['iar'] = {'ghbar': 0.7e-04, 'shift': -0.0} # O
 ######################
 #### ICAN current ###
 ######################
-cellRule['secs']['soma']['mechs']['ican'] = {'gbar': 0.0001, 'ratc': 0.8, 'ratC': 0.1} #gbar: 0.0003 # ratc: proportion coming from low-thresh pool (IT) # ratC: proportion coming from high-thresh pool (IL)
-cellRule['globals']['beta_ican'] = 0.003 		# correct re: jun.pdf & thesis (units: ms^-1)
-cellRule['globals']['cac_ican'] = 1.1e-04		# alpha = beta / (cac^x), alpha = 1.4e05 ms^-1 microM^-8
-cellRule['globals']['x_ican'] = 8				# correct re: jun.pdf, if x_ican == "n" (yes)
+cellRule['secs']['soma']['mechs']['icanINT'] = {'gbar': 0.0001, 'ratc': 0.8, 'ratC': 0.1} #gbar: 0.0003 # ratc: proportion coming from low-thresh pool (IT) # ratC: proportion coming from high-thresh pool (IL)
+cellRule['globals']['beta_icanINT'] = 0.003 		# correct re: jun.pdf & thesis (units: ms^-1)
+cellRule['globals']['cac_icanINT'] = 1.1e-04		# alpha = beta / (cac^x), alpha = 1.4e05 ms^-1 microM^-8
+cellRule['globals']['x_icanINT'] = 8				# correct re: jun.pdf, if x_icanINT == "n" (yes)
 ## trial line: 
-# cellRule['secs']['soma']['mechs']['ican']['gbar'] = 		# ORIG in THESIS: 1mS/cm2 # ORIG in sTI.hoc: 2e-05  #1e-7?? 
-# cellRule['globals']['cac_ican'] = 
+# cellRule['secs']['soma']['mechs']['icanINT']['gbar'] = 		# ORIG in THESIS: 1mS/cm2 # ORIG in sTI.hoc: 2e-05  #1e-7?? 
+# cellRule['globals']['cac_icanINT'] = 
 
 ######################
 #### IAHP current ####
@@ -103,12 +103,12 @@ cellRule['globals']['cac_iahp'] = 8e-04
 ####################
 #### IT current ####
 ####################
-cellRule['secs']['soma']['mechs']['it2'] = {'gcabar': 0.4e-04, 'shift1': 7}
-cellRule['globals']['shift2_it2'] = 0 		# correct re: jun.pdf & thesis 
-cellRule['globals']['mx_it2'] = 3.0			# correct re: jun.pdf & thesis
-cellRule['globals']['hx_it2'] = 1.5			# correct re: jun.pdf & thesis
-cellRule['globals']['sm_it2'] = 4.8 		# correct re: jun.pdf & thesis
-cellRule['globals']['sh_it2'] = 4.6			# correct re: jun.pdf & thesis
+cellRule['secs']['soma']['mechs']['it2INT'] = {'gcabar': 0.4e-04, 'shift1': 7}
+cellRule['globals']['shift2_it2INT'] = 0 		# correct re: jun.pdf & thesis 
+cellRule['globals']['mx_it2INT'] = 3.0			# correct re: jun.pdf & thesis
+cellRule['globals']['hx_it2INT'] = 1.5			# correct re: jun.pdf & thesis
+cellRule['globals']['sm_it2INT'] = 4.8 		# correct re: jun.pdf & thesis
+cellRule['globals']['sh_it2INT'] = 4.6			# correct re: jun.pdf & thesis
 ## trial line: 
 # cellRule['secs']['soma']['mechs']['it2']['gcabar'] = 		# ORIG in sTI.hoc: 4.0e-04
 
@@ -116,25 +116,25 @@ cellRule['globals']['sh_it2'] = 4.6			# correct re: jun.pdf & thesis
 #############################################################
 #### CALCIUM PUMP FOR "ca" ION POOL - associated with IT ####
 #############################################################
-cellRule['secs']['soma']['mechs']['cad'] = {'taur': 150, 'taur2': 80, 'cainf': 1e-8, 'cainf2': 5.3e-5, 'kt': 0e-6, 'kt2': 0e-7, 'k': 7.5e-3}
+cellRule['secs']['soma']['mechs']['cad_int'] = {'taur': 150, 'taur2': 80, 'cainf': 1e-8, 'cainf2': 5.3e-5, 'kt': 0e-6, 'kt2': 0e-7, 'k': 7.5e-3}
 ## TRIAL VALUES: 
-#cellRule['secs']['soma']['mechs']['cad']['k'] = 7.5e-3 #50					# ORIG: 7.5e-3
+#cellRule['secs']['soma']['mechs']['cad_int']['k'] = 7.5e-3 #50					# ORIG: 7.5e-3
 
 ####################
 #### IL current ####
 ####################
-cellRule['secs']['soma']['mechs']['ical'] = {'pcabar': 0.00009}
-cellRule['globals']['sh1_ical'] = -10
-cellRule['globals']['sh2_ical'] = 0
+cellRule['secs']['soma']['mechs']['icalINT'] = {'pcabar': 0.00009}
+cellRule['globals']['sh1_icalINT'] = -10
+cellRule['globals']['sh2_icalINT'] = 0
 ## trial line: 
-# cellRule['secs']['soma']['mechs']['ical']['pcabar'] = 			# ORIG: 9.0e-04
+# cellRule['secs']['soma']['mechs']['icalINT']['pcabar'] = 			# ORIG: 9.0e-04
 
 ##############################################################
 #### CALCIUM PUMP FOR "Ca" ION POOL -- associated with IL ####
 ##############################################################
-cellRule['secs']['soma']['mechs']['Cad'] = {'taur': 150, 'taur2': 80, 'Cainf': 1e-8, 'Cainf2': 5.2e-5, 'kt': 0e-6, 'kt2': 0e-7, 'k': 5e-3}
+cellRule['secs']['soma']['mechs']['Cad_int'] = {'taur': 150, 'taur2': 80, 'Cainf': 1e-8, 'Cainf2': 5.2e-5, 'kt': 0e-6, 'kt2': 0e-7, 'k': 5e-3}
 ## TRIAL VALUES:
-#cellRule['secs']['soma']['mechs']['Cad']['k'] = 7.5e-3 #50					# ORIG: 7.5e-3
+#cellRule['secs']['soma']['mechs']['Cad_int']['k'] = 7.5e-3 #50					# ORIG: 7.5e-3
 
 
 
@@ -150,7 +150,7 @@ cellRule['globals']['k2i0_k2_ion'] = 54.5 			# NEURON: h.k2i0_k2_ion = 54.5
 cellRule['globals']['k2o0_k2_ion'] = 2.5 			# NEURON: h.k2o0_k2_ion = 2.5 
 
 
-### Ca2+ ION VALUES FOR Cad.mod & IL CURRENT ### 
+### Ca2+ ION VALUES FOR high-thresh pool (Ca) & IL CURRENT ### 
 cellRule['globals']['Cai0_Ca_ion'] = 5e-5			# NEURON: h.Cai0_Ca_ion = 5e-5
 cellRule['globals']['Cao0_Ca_ion'] = 2				# NEURON: h.Cao0_Ca_ion = 2
 
