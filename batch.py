fc23b74108ce9a97c66d62189edf624d7f3739d1
salvadord <salvadordura@gmail.com>
2020-04-27 19:35:20 -0400

    v22_batch23 - missing weightNorms for CT5A and CT5B

--- batch.py
+++ batch.py
@@ -839,7 +839,7 @@ if __name__ == '__main__':
     #b = custom()
     #b = evolRates()
 
-    cellTypes = ['IT2', 'PV2', 'SOM2', 'VIP2', 'NGF2', 'IT3', 'ITP4', 'ITS4', 'IT5A', 'CT5A', 'IT5B', 'PT5B', 'CT5B', 'IT6', 'CT6', 'TC', 'HTC', 'IRE', 'TI']
+    #cellTypes = ['IT2', 'PV2', 'SOM2', 'VIP2', 'NGF2', 'IT3', 'ITP4', 'ITS4', 'IT5A', 'CT5A', 'IT5B', 'PT5B', 'CT5B', 'IT6', 'CT6', 'TC', 'HTC', 'IRE', 'TI']
 
     # b = bkgWeights(pops = cellTypes, weights = list(range(1,100)))
     # # b = fIcurve(pops=cellTypes) 
