28dfeb20f89725fcf9541e51887f5fd0d305ce54
salvadord <salvadordura@gmail.com>
2020-05-04 19:27:49 -0400

    v22_sim13 - bkg weights for all pops (E 40hz apical, I 40 Hz proximal); after manualScaling IT6, IPT4,ITS4

--- cfg.py
+++ cfg.py
@@ -57,7 +57,7 @@ cfg.recordStep = 0.1            ## Step size (in ms) to save data -- value from 
 # Saving
 #------------------------------------------------------------------------------
 
-cfg.simLabel = 'v22_sim13'
+cfg.simLabel = 'v22_sim14'
 cfg.saveFolder = 'data/v22_manualTune/'                	## Set file output name
 cfg.savePickle = False         	## Save pkl file
 cfg.saveJson = True           	## Save json file
