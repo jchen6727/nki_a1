0bb24e578b4b8c43031ed0120dba9aa4e06277af
salvadord <salvadordura@gmail.com>
2020-07-13 11:24:05 -0400

    v25_batch5 debug

--- cfg.py
+++ cfg.py
@@ -122,21 +122,6 @@ cfg.synWeightFractionII = [0.9, 0.1]  # SOM -> E GABAASlow to GABAB ratio (updat
 # Cortical
 cfg.addConn = 1
 
-
-    initCfg[('IELayerGain', '1-3')] = 0.7628339148790332 
-    initCfg[('IELayerGain', '4')] = 1.974325049287055 
-    initCfg[('IELayerGain', '5')] = 0.7753261834291882 
-    initCfg[('IELayerGain', '6')] = 1.5588140501085836 
-    initCfg[('IILayerGain', '1-3')] = 0.8006022233940763 
-    initCfg[('IILayerGain', '4')] = 1.999312334956534 
-    initCfg[('IILayerGain', '5')] = 0.9126340893010045 
-    initCfg[('IILayerGain', '6')] = 1.5704716674573778 
-    initCfg['EEGain'] = 1.9405846115921952 
-    initCfg['EIGain'] = 1.8600534795309025 
-    initCfg['corticoThalamicGain'] = 1.083986203608584 
-    initCfg['intraThalamicGain'] = 0.5531222327683913
-    initCfg['thalamoCorticalGain'] = 1.8483736535302833
-
 cfg.EEGain = 1.9405846115921952 
 cfg.EIGain = 1.8600534795309025 	
 cfg.IEGain = 1.0 #0.75
