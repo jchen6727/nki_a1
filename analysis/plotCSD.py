8505345d146896c9e288fe8b2e8ff6f73aeec5f8
salvadord <salvadordura@gmail.com>
2020-07-24 13:00:55 -0400

    optunaAnalysis

--- analysis/plotCSD.py
+++ analysis/plotCSD.py
@@ -1,17 +1,11 @@
 import utils
+from matplotlib import pyplot as plt
 
 allpops = ['NGF1', 'IT2', 'PV2', 'SOM2', 'VIP2', 'NGF2', 'IT3', 'SOM3', 'PV3', 'VIP3', 'NGF3', 'ITP4', 'ITS4',
 'PV4', 'SOM4', 'VIP4', 'NGF4', 'IT5A', 'CT5A', 'PV5A', 'SOM5A', 'VIP5A', 'NGF5A', 'IT5B', 'PT5B', 'CT5B', 'PV5B',
 'SOM5B', 'VIP5B', 'NGF5B', 'IT6', 'CT6', 'PV6', 'SOM6', 'VIP6', 'NGF6', 'TC', 'TCM', 'HTC', 'IRE', 'IREM', 'TI', 'IC']
 
-timeRange=[450,700]
-sim = utils.loadFromFile('../data/v25_batch4/v25_batch4_1_1.json')
-
-#sim.analysis.getCSD(minf=0.1, maxf=100, spacing_um=100, sampr=1./sim.cfg.recordStep)
-
-#sim.analysis.plotLFP(electrodes=[0,1,2,3], timeRange=timeRange, plots=['timeSeries'], saveFig='../data/v23_batch8/v23_batch8_0_0_0_0_%d_LFP.png' % (i))
-#sim.analysis.plotLFP(electrodes=[0,1,2,3], timeRange=timeRange, plots=['spectrogram'], saveFig='../data/v23_batch8/v23_batch8_0_0_0_0_%d_LFP_spect.png' % (i))
-
-# sim.analysis.plotCSD(electrodes=range(11), timeRange=timeRange, separation=1.2, plots=['timeSeries'], smooth=512, saveFig=' % (i))
-
-sim.analysis.plotCSD(empirical=True,  NHP =False, timeRange=timeRange, spacing_um=100, hlines=True, saveData=None, saveFig='../data/v25_batch4/v25_batch4_1_1_CSD.png', showFig=True, LFP_overlay=True)
+timeRange=[1100, 1400]
+sim = utils.loadFromFile('../data/v25_batch5/v25_batch5_0_0_2.json')
+sim.analysis.plotCSD(spacing_um=100, timeRange=timeRange, LFP_overlay=True, layer_lines=True, saveFig=1, showFig=0)
+plt.savefig('v25_batch5_0_0_2_1100-1400.png')   
--- cfg.py
+++ cfg.py
@@ -42,7 +42,7 @@ cfg.printPopAvgRates = [500, cfg.duration]
 #------------------------------------------------------------------------------
 # Recording 
 #------------------------------------------------------------------------------
-cfg.allpops = ['NGF1', 'IT2', 'PV2', 'SOM2', 'VIP2', 'NGF2', 'IT3', 'SOM3', 'PV3', 'VIP3', 'NGF3', 'ITP4', 'ITS4', 'PV4', 'SOM4', 'VIP4', 'NGF4', 'IT5A', 'CT5A', 'PV5A', 'SOM5A', 'VIP5A', 'NGF5A', 'IT5B', 'PT5B', 'CT5B', 'PV5B', 'SOM5B', 'VIP5B', 'NGF5B', 'IT6', 'CT6', 'PV6', 'SOM6', 'VIP6', 'NGF6', 'TC', 'TCM', 'HTC', 'IRE', 'IREM', 'TI', 'IC']
+cfg.allpops = ['NGF1', 'IT2', 'PV2', 'SOM2', 'VIP2', 'NGF2', 'IT3', 'PV3', 'SOM3', 'VIP3', 'NGF3', 'ITP4', 'ITS4', 'PV4', 'SOM4', 'VIP4', 'NGF4', 'IT5A', 'CT5A', 'PV5A', 'SOM5A', 'VIP5A', 'NGF5A', 'IT5B', 'PT5B', 'CT5B', 'PV5B', 'SOM5B', 'VIP5B', 'NGF5B', 'IT6', 'CT6', 'PV6', 'SOM6', 'VIP6', 'NGF6', 'TC', 'TCM', 'HTC', 'IRE', 'IREM', 'TI', 'IC']
 
 alltypes = ['NGF1', 'IT2', 'PV2', 'SOM2', 'VIP2', 'ITS4', 'PT5B', 'TC', 'HTC', 'IRE', 'TI']
 
