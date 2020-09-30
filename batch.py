a5eb6e1c4b1898987c740c34e39d8add3a7757a9
salvadord <salvadordura@gmail.com>
2020-09-30 13:07:02 -0400

    v29_batch1

--- batch.py
+++ batch.py
@@ -1255,7 +1255,7 @@ if __name__ == '__main__':
 
     cellTypes = ['IT2', 'PV2', 'SOM2', 'VIP2', 'NGF2', 'IT3', 'ITP4', 'ITS4', 'IT5A', 'CT5A', 'IT5B', 'PT5B', 'CT5B', 'IT6', 'CT6', 'TC', 'HTC', 'IRE', 'TI']
 
-    b = custom()
+    #b = custom()
     # b = evolRates()
     # b = asdRates()
     b = optunaRates()
