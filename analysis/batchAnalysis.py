4c704b19e8597f4bf00bcc073820193e68378ad1
ericagriffith <ericaygriffith@gmail.com>
2020-10-27 16:50:27 -0400

    reduced length of soma and changed resulting dend and soma pt3d values

--- analysis/batchAnalysis.py
+++ analysis/batchAnalysis.py
@@ -16,7 +16,7 @@ from batchAnalysisPlotCombined import *
 # Main code
 if __name__ == '__main__': 
     dataFolder = '../data/'
-    batchLabel = 'v29_trial13425_L4fIcurves'  # 'v50_batch1' #
+    batchLabel = 'v29_trial13425_L4fIcurves_somaReduc'  # 'v50_batch1' #
     #batchLabels = ['v103_batch3/gen_%d' % (i) for i in range(68)]
     loadAll = 0
 
