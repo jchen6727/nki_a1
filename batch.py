803663742360e306bcd9eff2de0b61fbaa014358
salvadord <salvadordura@gmail.com>
2020-11-16 22:12:23 -0500

    v32_batch1

--- batch.py
+++ batch.py
@@ -1474,7 +1474,7 @@ if __name__ == '__main__':
     #b = bkgWeights2D(pops = ['ITS4'], weights = list(np.arange(0,150,10)))
     #b = fIcurve(pops=['ITS4']) 
 
-    b.batchLabel = 'v32_batch1'
+    b.batchLabel = 'v32_batch1' 
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_slurm_gcp') #'hpc_slurm_gcp') #'mpi_bulletin') #'hpc_slurm_gcp')
