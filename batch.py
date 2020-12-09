9507e67d13dbc339dbcc850da352a7acd94974c9
salvadord <salvadordura@gmail.com>
2020-12-08 22:18:22 -0500

    v32_batch8

--- batch.py
+++ batch.py
@@ -1362,7 +1362,7 @@ def optunaRatesLayers():
         'maxFitness': fitnessFuncArgs['maxFitness'],
         'maxiters':     1e6,    #    Maximum number of iterations (1 iteration = 1 function evaluation)
         'maxtime':      None,    #    Maximum time allowed, in seconds
-        'maxiter_wait': 40,
+        'maxiter_wait': 45,
         'time_sleep': 60,
         'popsize': 1  # unused - run with mpi 
     }
