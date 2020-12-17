0f34812feb400ff2632bff4464d5393ee0be803f
ericagriffith <ericaygriffith@gmail.com>
2020-12-17 15:41:43 -0500

    adding dataAnalysis.py file to analysis -- added several functions -- prior rejected commits described in nb

---
+++ analysis/dataAnalysis.py
@@ -0,0 +1,141 @@
+"""
+dataAnalysis.py 
+
+Code to anlayze non-human primate data 
+
+Contributors: ericaygriffith@gmail.com, samnemo@gmail.com
+"""
+
+
+## IMPORTS 
+import sys
+import os
+import h5py									# for rdmat() and getTriggerTimes()
+import numpy as np
+import downsample
+from collections import OrderedDict
+from filter import lowpass,bandpass 		 #for getbandpass()
+
+
+## FUNCTIONS ## 
+
+### Originally in rdmat.py ### 
+def rdmat (fn,samprds=0):  
+  fp = h5py.File(fn,'r') # open the .mat / HDF5 formatted data
+  sampr = fp['craw']['adrate'][0][0] # original sampling rate
+  print('fn:',fn,'sampr:',sampr,'samprds:',samprds)
+  dt = 1.0 / sampr # time-step in seconds
+  dat = fp['craw']['cnt'] # cnt record stores the electrophys data
+  npdat = np.zeros(dat.shape)
+  tmax = ( len(npdat) - 1.0 ) * dt # use original sampling rate for tmax - otherwise shifts phase
+  dat.read_direct(npdat) # read it into memory; note that this LFP data usually stored in microVolt
+  npdat *= 0.001 # convert microVolt to milliVolt here
+  fp.close()
+  if samprds > 0.0: # resample the LFPs
+    dsfctr = sampr/samprds
+    dt = 1.0 / samprds
+    siglen = max((npdat.shape[0],npdat.shape[1]))
+    nchan = min((npdat.shape[0],npdat.shape[1]))
+    npds = [] # zeros((int(siglen/float(dsfctr)),nchan))
+    # print dsfctr, dt, siglen, nchan, samprds, ceil(int(siglen / float(dsfctr))), npds.shape
+    for i in range(nchan): 
+      print('resampling channel', i)
+      npds.append(downsample.downsample(npdat[:,i], sampr, samprds))
+    npdat = np.array(npds)
+    npdat = npdat.T
+    sampr = samprds
+  tt = np.linspace(0,tmax,len(npdat)) # time in seconds
+  return sampr,npdat,dt,tt # npdat is LFP in units of milliVolt
+
+
+# bandpass filter the items in lfps. lfps is a list or numpy array of LFPs arranged spatially by column
+### Originally in load.py ### 
+def getbandpass (lfps,sampr,minf=0.05,maxf=300):
+  datband = []
+  for i in range(len(lfps[0])): datband.append(bandpass(lfps[:,i],minf,maxf,df=sampr,zerophase=True))
+  datband = numpy.array(datband)
+  return datband
+
+
+# Vaknin correction for CSD analysis
+# Allows CSD to be performed on all N contacts instead of N-2 contacts
+# See Vaknin et al (1989) for more details
+### Originally in load.py ### 
+def Vaknin(x):
+    # Preallocate array with 2 more rows than input array
+    x_new = np.zeros((x.shape[0]+2, x.shape[1]))
+
+    # Duplicate first and last row of x into first and last row of x_new
+    x_new[0, :] = x[0, :]
+    x_new[-1, :] = x[-1, :]
+
+    # Duplicate all of x into middle rows of x_new
+    x_new[1:-1, :] = x
+
+    return x_new
+
+
+# get CSD - first do a lowpass filter. lfps is a list or numpy array of LFPs arranged spatially by column
+def getCSD (lfps,sampr,spacing_um,minf=0.05,maxf=300):
+
+  # convert from uV to mV
+  lfps = lfps/1000
+
+  datband = getbandpass(lfps,sampr,minf,maxf)
+  if datband.shape[0] > datband.shape[1]: # take CSD along smaller dimension
+    ax = 1
+  else:
+    ax = 0
+
+  spacing_mm = spacing_um/1000
+  # when drawing CSD make sure that negative values (depolarizing intracellular current) drawn in red,
+  # and positive values (hyperpolarizing intracellular current) drawn in blue
+  CSD = -numpy.diff(datband,n=2,axis=ax)/spacing_mm**2 # now each column (or row) is an electrode -- CSD along electrodes
+
+  CSD = Vaknin(CSD)
+
+  return CSD
+
+
+#
+def getTriggerTimes (fn):
+  fp = h5py.File(fn,'r')
+  hdf5obj = fp['trig/anatrig']
+  x = np.array(fp[hdf5obj.name])
+  val = [y[0] for y in fp[x[0,0]].value]
+  fp.close()
+  return val  
+
+#
+def loadfile (fn,samprds,spacing_um=100):
+  # load a .mat data file (fn) using sampling rate samprds (should be integer factor of original sampling rate (44000)),
+  # returns: sampr is sampling rate after downsampling
+  #          LFP is laminar local field potential data
+  #          dt is time-step (redundant with sampr)
+  #          tt is time array (in seconds)
+  #          CSD is laminar current source density signal
+  #          trigtimes is array of stimulus trigger indices (indices into arrays)
+  sampr,LFP,dt,tt=rdmat(fn,samprds=samprds) # # samprds = 11000.0 # downsampling to this frequency
+  sampr,dt,tt[0],tt[-1] # (2000.0, 0.001, 0.0, 1789.1610000000001)
+  CSD = getCSD(LFP,sampr,spacing_um)
+  divby = 44e3 / samprds
+  trigtimes = None
+  try: # not all files have stimuli
+    trigtimes = [int(round(x)) for x in np.array(getTriggerTimes(fn)) / divby] # divby since downsampled signals by factor of divby
+  except:
+    pass
+  #trigIDs = getTriggerIDs(fn)
+  LFP = LFP.T # make sure each row is a channel
+  return sampr,LFP,dt,tt,CSD,trigtimes
+
+
+
+
+
+
+# MAIN CODE 
+if __name__ == '__main__':
+
+	fileName = '../data/NHPdata/click/contproc/1-bu001002015@os_eye06_20.mat' # SPONT: '1-bu001002017@os_eye06_20.mat' # CLICK: '1-bu001002015@os_eye06_20.mat' #'1-rb067068029@os.mat'
+
+
