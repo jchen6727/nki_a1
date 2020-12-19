9e06918ee4892d06f346962ca8cd69f7a3dc61be
salvadord <salvadordura@gmail.com>
2020-12-18 20:23:09 -0500

    v32_batch16

--- batch.py
+++ batch.py
@@ -1202,7 +1202,7 @@ def optunaRatesLayers():
     # parameters
     params = specs.ODict()
 
-    rangeV = 0.25
+    rangeV = 1.0
     rangeV2 = 0.1
     minV = 0.1
     maxV = 4.0
@@ -1450,16 +1450,16 @@ if __name__ == '__main__':
 
     cellTypes = ['IT2', 'PV2', 'SOM2', 'VIP2', 'NGF2', 'IT3', 'ITP4', 'ITS4', 'IT5A', 'CT5A', 'IT5B', 'PT5B', 'CT5B', 'IT6', 'CT6', 'TC', 'HTC', 'IRE', 'TI']
 
-    b = custom()
+    # b = custom()
     # b = evolRates()
     # b = asdRates()
     # b = optunaRates()
-    # b = optunaRatesLayers()
+    b = optunaRatesLayers()
     # b = bkgWeights(pops = cellTypes, weights = list(np.arange(1,100)))
     #b = bkgWeights2D(pops = ['ITS4'], weights = list(np.arange(0,150,10)))
     #b = fIcurve(pops=['ITS4']) 
 
-    b.batchLabel = 'v32_batch15' 
+    b.batchLabel = 'v32_batch16' 
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_slurm_gcp') #'hpc_slurm_gcp') #'mpi_bulletin') #'hpc_slurm_gcp')
