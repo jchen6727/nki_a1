140b8999d1d5a35fc48d119b9683339a1c7e6a55
ericagriffith <ericaygriffith@gmail.com>
2021-07-26 09:47:36 -0400

    adding recording to quarter density for 3 cells

--- batch.py
+++ batch.py
@@ -765,8 +765,8 @@ def custom():
     initCfg[('analysis', 'plotCSD', 'timeRange')] = [1500, 1700]
 
 
-    ## save LFP trace from first cell of each pop 
-    #initCfg['saveLFPCells'] = [('IT2', [0]), ('PV2', [0]), ('SOM2', [0]), ('VIP2', [0]), ('NGF2', [0]),
+    # save LFP trace from first cell of each pop 
+    initCfg['saveLFPCells'] = [('IT2', [0]), ('ITS4', [0]), ('PV3', [0])] #[('IT2', [0]), ('PV2', [0]), ('SOM2', [0]), ('VIP2', [0]), ('NGF2', [0]),
                                 # ('IT3', [0]), ('SOM3', [0]), ('PV3', [0]), ('VIP3', [0]), ('NGF3', [0]),
                                 # ('ITP4', [0]), ('ITS4', [0]), ('SOM4', [0]), ('PV4', [0]), ('VIP4', [0]), ('NGF4', [0]),
                                 # ('IT5A', [0]), ('CT5A', [0]), ('SOM5A', [0]), ('PV5A', [0]), ('VIP5A', [0]), ('NGF5A', [0]), 
@@ -2780,7 +2780,7 @@ if __name__ == '__main__':
     #b = bkgWeights2D(pops = ['ITS4'], weights = list(np.arange(0,150,10)))
     #b = fIcurve(pops=['IT3','CT5']) 
 
-    b.batchLabel = 'v34_batch27_quarterDensity'  # lowDensity == 0.075 == 7.5% density 
+    b.batchLabel = 'v34_batch27_QD_rec'  # lowDensity == 0.075 == 7.5% density 
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_slurm_gcp') #'hpc_slurm_gcp') #'mpi_bulletin') #'hpc_slurm_gcp')
