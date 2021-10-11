d17b86382348a5ebc5edd82f60367f8293cffb22
ericagriffith <ericaygriffith@gmail.com>
2021-10-11 12:57:28 -0400

    L5 redo

--- batch.py
+++ batch.py
@@ -2856,7 +2856,7 @@ if __name__ == '__main__':
     #b = bkgWeights2D(pops = ['ITS4'], weights = list(np.arange(0,150,10)))
     #b = fIcurve(pops=['IT3','CT5']) 
 
-    b.batchLabel = 'v34_batch27_0_0_LFP_L5' 
+    b.batchLabel = 'v34_batch27_0_0_LFP_L5_REDO' 
     b.saveFolder = 'data/'+b.batchLabel
 
     setRunCfg(b, 'hpc_slurm_gcp') #'hpc_slurm_gcp') #'mpi_bulletin') #'hpc_slurm_gcp')
