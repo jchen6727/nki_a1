502f3a4f134e7f426cfb5e9c2d4e5b3d77e980ac
salvadord <salvadordura@gmail.com>
2021-10-20 16:57:17 -0400

    v34_batch50 - debug

--- cfg.py
+++ cfg.py
@@ -53,7 +53,7 @@ cfg.recordStep = 0.1            ## Step size (in ms) to save data -- value from 
 
 # cfg.recordLFP = [[100, y, 100] for y in range(0, 2000, 100)] #+[[100, 2500, 200], [100,2700,200]]
 cfg.recordLFP = [[x, 1000, 100] for x in range(100, 2200, 200)] #+[[100, 2500, 200], [100,2700,200]]
-cfg.saveLFPPops = cfg.allpops
+cfg.saveLFPPops = ['IT3', 'PV3', 'SOM3', 'VIP3', 'ITP4', 'ITS4', 'PV4', 'SOM4', 'VIP4', 'IT5B', 'PT5B', 'PV5B', 'SOM5B', 'VIP5B']
 
 #------------------------------------------------------------------------------
 # Saving
