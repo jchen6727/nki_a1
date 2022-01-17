4480930a496ab0d91bd1bd810e7afcdde96e5181
ericagriffith <ericaygriffith@gmail.com>
2022-01-17 18:42:50 -0500

    checking lfp calculations, total and by electrode

--- analysis/sandbox.py
+++ analysis/sandbox.py
@@ -1,6 +1,76 @@
-# from netpyne import sim
+from netpyne import sim
+import numpy as np
+#from simDataAnalysis import *
 
 
+
+######################## DEBUGGING LFP HEAT PLOTS ########################
+
+waveletInfo = {'delta': {'dataFile': 'A1_v34_batch65_v34_batch65_0_0_data.pkl', 'timeRange': [1480, 2520]},
+	'beta': {'dataFile': 'A1_v34_batch65_v34_batch65_1_1_data.pkl', 'timeRange': [456, 572]}, 
+	'alpha': {'dataFile': 'A1_v34_batch65_v34_batch65_1_1_data.pkl', 'timeRange': [3111, 3325]}, 
+	'theta': {'dataFile': 'A1_v34_batch65_v34_batch65_2_2_data.pkl', 'timeRange': [2785, 3350]}}
+
+freqBand = 'delta'
+based = '/Users/ericagriffith/Desktop/NEUROSIM/A1/data/simDataFiles/spont/' 
+
+dataFile = based + waveletInfo[freqBand]['dataFile']
+timeRange = waveletInfo[freqBand]['timeRange']
+
+print('timeRange = ' + str(timeRange))
+print('dataFile = ' + str(dataFile))
+
+sim.load(dataFile, instantiate=False)
+
+
+
+### thal pops
+thalPops = ['TC', 'TCM', 'HTC', 'IRE', 'IREM', 'TI', 'TIM']
+allPops = list(sim.net.allPops.keys())
+pops = [pop for pop in allPops if pop not in thalPops] 			## exclude thal pops 
+## output --> >>> pops
+# ['NGF1', 'IT2', 'SOM2', 'PV2', 'VIP2', 'NGF2', 'IT3', 'SOM3', 'PV3', 'VIP3', 'NGF3', 'ITP4', 'ITS4', 'SOM4', 'PV4', 'VIP4', 'NGF4', 'IT5A', 'CT5A', 'SOM5A', 'PV5A', 'VIP5A', 'NGF5A', 'IT5B', 'CT5B', 'PT5B', 'SOM5B', 'PV5B', 'VIP5B', 'NGF5B', 'IT6', 'CT6', 'SOM6', 'PV6', 'VIP6', 'NGF6']
+
+### play with lfp data 
+pop = 'NGF1'
+
+popLFPdata_TOTAL = sim.allSimData['LFPPops'][pop]
+print('dims of popLFPdata: ' + str(popLFPdata_TOTAL.shape)) 			 # (230000, 20)
+
+
+popLFPdata = np.array(sim.allSimData['LFPPops'][pop])[int(timeRange[0]/sim.cfg.recordStep):int(timeRange[1]/sim.cfg.recordStep),:]
+print('dims of new popLFPdata: ' + str(popLFPdata.shape))
+# dims of new popLFPdata: (20800, 20)
+
+totalAvg = np.average(popLFPdata)
+print('totalAvg: ' + str(totalAvg))
+totalPeak = np.amax(popLFPdata)
+print('totalPeak: ' + str(totalPeak))
+
+
+
+### calculate avg and peak 
+# for pop in pops:
+# 	lfpPopData[pop] = {}
+
+# 	popLFPdata = np.array(sim.allSimData['LFPPops'][pop])[int(timeRange[0]/sim.cfg.recordStep):int(timeRange[1]/sim.cfg.recordStep),:]
+
+# 	lfpPopData[pop]['totalLFP'] = popLFPdata
+# 	lfpPopData[pop]['totalAvg'] = np.average(popLFPdata)
+# 	lfpPopData[pop]['totalPeak'] = np.amax(popLFPdata)
+
+
+# 	for elec in evalElecs:
+# 		elecKey = 'elec' + str(elec)
+# 		lfpPopData[pop][elecKey] = {}
+# 		lfpPopData[pop][elecKey]['LFP'] = popLFPdata[:, elec]
+# 		lfpPopData[pop][elecKey]['avg'] = np.average(popLFPdata[:, elec])
+# 		lfpPopData[pop][elecKey]['peak'] = np.amax(popLFPdata[:, elec])
+
+
+
+
+###############################################################
 # pathToFiles = '/Users/ericagriffith/Desktop/NEUROSIM/A1/data/simDataFiles/spont/'
 
 # ### batch 57 
@@ -39,14 +109,14 @@
 #### NOW COMPARE LFP #### 
 
 ##### THAL POP DEBUGGING #####
-pops = ['NGF1', 'IT2', 'SOM2', 'PV2', 'VIP2', 'NGF2', 'IT3', 'SOM3', 'PV3', 'VIP3', 'NGF3', 'ITP4', 'ITS4', 'SOM4', 'PV4', 'VIP4', 'NGF4', 'IT5A', 'CT5A', 'SOM5A', 'PV5A', 'VIP5A', 'NGF5A', 'IT5B', 'CT5B', 'PT5B', 'SOM5B', 'PV5B', 'VIP5B', 'NGF5B', 'IT6', 'CT6', 'SOM6', 'PV6', 'VIP6', 'NGF6', 'TC', 'TCM', 'HTC', 'IRE', 'IREM', 'TI', 'TIM']
-thalPops = ['TC', 'TCM', 'HTC', 'IRE', 'IREM', 'TI', 'TIM']
+# pops = ['NGF1', 'IT2', 'SOM2', 'PV2', 'VIP2', 'NGF2', 'IT3', 'SOM3', 'PV3', 'VIP3', 'NGF3', 'ITP4', 'ITS4', 'SOM4', 'PV4', 'VIP4', 'NGF4', 'IT5A', 'CT5A', 'SOM5A', 'PV5A', 'VIP5A', 'NGF5A', 'IT5B', 'CT5B', 'PT5B', 'SOM5B', 'PV5B', 'VIP5B', 'NGF5B', 'IT6', 'CT6', 'SOM6', 'PV6', 'VIP6', 'NGF6', 'TC', 'TCM', 'HTC', 'IRE', 'IREM', 'TI', 'TIM']
+# thalPops = ['TC', 'TCM', 'HTC', 'IRE', 'IREM', 'TI', 'TIM']
 
-pops_new = [pop for pop in pops if pop not in thalPops]
+# pops_new = [pop for pop in pops if pop not in thalPops]
 
-for pop in pops:
-	if any(elem == pop for elem in thalPops):
-		print('redundant pop: ' + pop)
+# for pop in pops:
+# 	if any(elem == pop for elem in thalPops):
+# 		print('redundant pop: ' + pop)
 		#pops.remove(pop)
 
 
