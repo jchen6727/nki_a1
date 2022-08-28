1086369ef693d3ee94b9dc1c7d7c4c303f0adff8
ericagriffith <ericaygriffith@gmail.com>
2022-08-28 17:34:27 -0400

    adding more coresPerNode and increasing walltime

--- batch.py
+++ batch.py
@@ -3056,9 +3056,9 @@ def setRunCfg(b, type='mpi_bulletin'):
     elif type=='hpc_slurm_cineca':         ## FILL THIS IN
         b.runCfg = {'type': 'hpc_slurm',
             'allocation': 'icei_H_King',
-            'walltime': '00:15:00',           # g100_qos_dbg : 2 hrs           # noQOS: 24 hrs 
+            'walltime': '00:20:00',           # g100_qos_dbg : 2 hrs           # noQOS: 24 hrs 
             'nodes': 2,                       # g100_qos_dbg : max 2 nodes     # noQOS: max 32 nodes 
-            'coresPerNode': 24,               # g100_qos_dbg : nodes*coresPerNode = 96 MAX       
+            'coresPerNode': 48,               # g100_qos_dbg : nodes*coresPerNode = 96 MAX       
             'partition': 'g100_usr_prod',
             'qos': None, #'noQOS',           # g100_qos_dbg  # noQOS
             'email': 'erica.griffith@downstate.edu',
@@ -3108,7 +3108,7 @@ if __name__ == '__main__':
     # b = bkgWeights2D(pops = ['ITS4'], weights = list(np.arange(0,150,10)))
     # b = fIcurve(pops=['IT3','CT5']) 
 
-    b.batchLabel = 'v34_batch67_CINECA_TRIAL_fullScale_0'  #'v34_batch67_XSEDE_TRIAL_0'
+    b.batchLabel = 'v34_batch67_CINECA_TRIAL_fullScale_1'  #'v34_batch67_XSEDE_TRIAL_0'
     cinecaScratch = '/g100_scratch/userexternal/egriffit/A1/'
     b.saveFolder = cinecaScratch + b.batchLabel         #'data/'+b.batchLabel
 
