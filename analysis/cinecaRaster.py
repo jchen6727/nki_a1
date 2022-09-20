6bc64b58892a03dd7a323db6107ff7cd6f0bc878
ericagriffith <ericaygriffith@gmail.com>
2022-09-20 14:57:36 -0400

    added for loop to raster plot file to save all at once

--- analysis/cinecaRaster.py
+++ analysis/cinecaRaster.py
@@ -1,38 +1,51 @@
 from netpyne import sim 
 from matplotlib import pyplot as plt 
+import os 
 
 
 basedir = '../data/simDataFiles/speech/v34_batch_eegSpeech_CINECA_trial_0/'
-fn = 'v34_batch_eegSpeech_CINECA_trial_0_2_data.pkl'
+# fn = 'v34_batch_eegSpeech_CINECA_trial_0_2_data.pkl'
 # fn = '../data/simDataFiles/speech/v34_batch_eegSpeech_CINECA_trial_0/v34_batch_eegSpeech_CINECA_trial_0_3_data.pkl'
 
-fullFilename = basedir + fn
 
-sim.load(fullFilename, instantiate=False)
-print('Loaded ' + fn)
+allFiles = os.listdir(basedir)
 
-orderBy = ['pop']
+pklFiles = []
+for file in allFiles:
+	if '_data.pkl' in file:
+		pklFiles.append(file)
 
-timeRange = [500,1500]
 
-fig1 = sim.analysis.plotRaster(include=['allCells'], timeRange=timeRange, labels='legend', 
-	popRates=False, orderInverse=True, lw=0, markerSize=12, marker='.',  
-	showFig=0, saveFig=0, figSize=(9*0.95, 13*0.9), orderBy=orderBy)
+for fn in pklFiles:
+	fullFilename = basedir + fn
 
-print('RASTER PLOTTED')
+	sim.load(fullFilename, instantiate=False)
+	print('Loaded ' + fn)
 
-ax = plt.gca()
+	orderBy = ['pop']
 
-[i.set_linewidth(0.5) for i in ax.spines.values()] # make border thinner
+	timeRange = [500,1500]
 
-plt.xticks(timeRange, ['0', '1'])
-plt.yticks([0, 5000, 10000], [0, 5000, 10000])
+	fig1 = sim.analysis.plotRaster(include=['allCells'], timeRange=timeRange, labels='legend', 
+		popRates=False, orderInverse=True, lw=0, markerSize=12, marker='.',  
+		showFig=0, saveFig=0, figSize=(9*0.95, 13*0.9), orderBy=orderBy)
 
-plt.ylabel('Neuron ID') #Neurons (ordered by NCD within each pop)')
-plt.xlabel('Time (s)')
+	print('RASTER PLOTTED')
 
-plt.title('')
-rasterFile=basedir+'speechRaster_0_2.png'
-plt.savefig(rasterFile, dpi=300)
+	ax = plt.gca()
 
-print('RASTER SAVED')
+	[i.set_linewidth(0.5) for i in ax.spines.values()] # make border thinner
+
+	plt.xticks(timeRange, ['0', '1'])
+	plt.yticks([0, 5000, 10000], [0, 5000, 10000])
+
+	plt.ylabel('Neuron ID') #Neurons (ordered by NCD within each pop)')
+	plt.xlabel('Time (s)')
+
+	plt.title('')
+
+	rasterFilename = fn.split('_data.pkl')[0]
+	rasterFile = basedir + rasterFilename + '_RASTER.png' #'speechRaster_0_2.png'
+	plt.savefig(rasterFile, dpi=300)
+
+	print('RASTER SAVED')
