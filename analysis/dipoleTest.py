32ea1a3e0fdfcd29568220693c41125543664889
ericagriffith <ericaygriffith@gmail.com>
2022-09-23 19:01:33 -0400

    raster for new speech runs

--- analysis/dipoleTest.py
+++ analysis/dipoleTest.py
@@ -1,7 +1,7 @@
 from simdat import *
 import os
 
-basedir = '/g100_scratch/userexternal/egriffit/A1/v34_batch_eegSpeech_CINECA_trial_3/' # CINECA
+basedir = '/g100_scratch/userexternal/egriffit/A1/v34_batch_eegSpeech_CINECA_trial_4/' # CINECA
 # basedir = '/Users/ericagriffith/Desktop/NEUROSIM/A1/data/simDataFiles/speech/v34_batch_eegSpeech_CINECA_trial_1/'
 
 allFiles = os.listdir(basedir)
@@ -20,7 +20,7 @@ for fn in pklFiles:
 
 	print('loaddat run on ' + fn)
 
-	rasterFilename = fullFilename.split('_data.pkl')[0] + '_rasterTest.png'
+	rasterFilename = fullFilename.split('_data.pkl')[0] + '_raster.png'
 	drawraster(dspkT,dspkID,dnumc,tlim=None,msz=0.5,skipstim=False,drawlegend=True,saveFig=True,rasterFile=rasterFilename) # skipstim=True
 	## ^^^ HOW TO SAVE?? 
 
