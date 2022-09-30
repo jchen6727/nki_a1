4d364b8a98edfcf350a81d2cd4b24d5e267450aa
ericagriffith <ericaygriffith@gmail.com>
2022-09-30 17:30:35 -0400

    fixing typo in shell script

--- analysis/dipoleTest.sh
+++ analysis/dipoleTest.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=loadCineca
+#SBATCH --job-name=dipoleTest
 #SBATCH -A icei_H_King
 #SBATCH --partition=g100_usr_prod
 #SBATCH --qos=g100_qos_dbg
 #SBATCH -t 1:30:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH -o testDipole.run
-#SBATCH -e testDipole.err
+#SBATCH --ntasks-per-node=2
+#SBATCH -o dipoleTest.run
+#SBATCH -e dipoleTest.err
 #SBATCH --mail-user=erica.griffith@downstate.edu
 #SBATCH --mail-type=end
 
@@ -15,6 +15,6 @@ source ~/.bashrc
 
 cd /g100/home/userexternal/egriffit/A1/analysis/
 
-srun python testDipole.py
+srun python dipoleTest.py
 
 wait
