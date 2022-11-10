e639f29ae0c63e86f476eb02ef2b0915dcdc8ca1
ericagriffith <ericaygriffith@gmail.com>
2022-11-10 17:05:24 -0500

    script for visualizing top population activity for osc events

---
+++ analysis/topPops.py
@@ -0,0 +1,140 @@
+import json
+#import pickle
+import pickle5 as pickle
+import os
+
+
+###################
+#### FUNCTIONS ####
+###################
+
+##################################################
+def topIndividualPops(topPopsData, freqBand, region):
+	## OUTPUT: bar graph with the top represented population(s) for given osc event subtype
+	#### topPopsData	: dict 
+	#### freqBand 		: str
+	#### region 		: str 
+
+	eventIndices = list(topPopsData[freqBand][region].keys())
+
+	popCounts = {}
+
+	for eventIdx in eventIndices:
+		topPops = topPopsData[freqBand][region][eventIdx]
+		for pop in topPops:
+			if pop in popCounts.keys():
+				popCounts[pop] += 1 
+			else:
+				popCounts[pop] = 1
+
+
+
+	popCountsSorted = {k: v for k, v in sorted(popCounts.items(), key=lambda item: item[1], reverse=True)}
+
+
+	return popCountsSorted
+
+
+
+
+
+#############################################
+# def topPopGroups(freqBand=None, region=None):
+	## OUTPUT: bar graph with the most popular trios for given osc event subtype
+
+
+
+
+################################################
+# def popSourceSink(oscEventData,freqBand, region):
+	## OUTPUT: Identify / Visualize the sources and sinks for given osc event subtype
+
+	### oscEventData[freqBand][region][subject][eventIdx]['maxPops_avgCSD']['elec'][pop] = CSD value
+
+
+
+
+
+################################################
+############# MAIN CODE BLOCK ##################
+################################################
+
+based = '/Users/ericagriffith/Desktop/NEUROSIM/A1/data/topPops/ECortPops/'
+
+
+os.chdir(based)
+
+allFiles = os.listdir()
+
+topPopsFiles = []
+oscEventFiles = []
+
+for file in allFiles:
+	if 'topPops' in file:
+		topPopsFiles.append(file)
+	if 'oscEventInfo' in file:
+		oscEventFiles.append(file)
+
+# print(topPopsFiles)
+# print(oscEventFiles)
+
+
+## LOAD TOP POPS DATA
+topPopsData = {}
+for topPopFile in topPopsFiles:
+	f = open(topPopFile)
+	data = json.load(f)
+	topPopsData.update(data)
+
+## LOAD OSC EVENT INFO INTO DICT 
+oscEventData = {}
+for oscEventFile in oscEventFiles:
+	with open(oscEventFile, 'rb') as handle:
+		data = pickle.load(handle)
+	oscEventData.update(data)
+
+
+### Synthesize findings from topPops Data ###
+regions = ['supra', 'gran', 'infra']
+freqBands = ['delta', 'theta', 'alpha', 'beta']
+
+ECortPops = ['IT2', 
+			 'IT3', 
+			 'ITP4', 'ITS4', 
+			 'IT5A', 'CT5A', 
+			 'IT5B', 'CT5B', 'PT5B', 
+			 'IT6', 'CT6']
+
+
+popCounts = topIndividualPops(topPopsData, 'theta', 'supra')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
