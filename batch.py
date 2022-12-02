35ff688ba131d2324c4e8598340154e788c07e82
ericagriffith <ericaygriffith@gmail.com>
2022-12-02 16:05:25 -0500

    changing path to .mat file for speech input

--- batch.py
+++ batch.py
@@ -866,7 +866,7 @@ def custom_speech(filename):
     #initCfg[('analysis', 'plotCSD', 'timeRange')] = [1500, 1700]
 
     # 'startTime': 2500, 
-    initCfg['ICThalInput'] = {'file': 'data/ICoutput/Speech_trials/ICoutput_CF_9600_10400_wav_01_ba_peter_AN.mat', 
+    initCfg['ICThalInput'] = {'file': 'data/ICoutput/speechTrials/ICoutput_CF_9600_10400_wav_01_ba_peter_AN.mat', 
                             'startTime': 2500, 
                             'weightE': 0.25,#1.0, 
                             'weightI': 0.25,#1.0, 
