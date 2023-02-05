63ec8744814ee7dd8a1eda9b48d7d1ff6b49cb1e
ericagriffith <ericaygriffith@gmail.com>
2023-02-05 16:16:23 -0500

    cleanup adaptation plotting code

--- analysis/adaptation.py
+++ analysis/adaptation.py
@@ -10,102 +10,95 @@ import numpy as np
 basedir = '/Users/ericagriffith/Desktop/NEUROSIM/A1/data/simDataFiles/tone/pureTone_CINECA_v36_CF500_tone500_SOA624/'
 #basedir = '/Users/ericagriffith/Desktop/NEUROSIM/A1/data/simDataFiles/tone/pureTone_CINECA_v36_CF500_tone500_SOA200/'
 
-# filename
-filename = 'pureTone_CINECA_v36_CF500_tone500_SOA624_data.pkl' #'pureTone_CINECA_v36_CF500_tone500_SOA200_data.pkl' #'REDO_BBN_CINECA_v36_5656BF_624SOA_0_0_0_data.pkl'
+filename = 'pureTone_CINECA_v36_CF500_tone500_SOA624_data.pkl' 
+#filename = 'pureTone_CINECA_v36_CF500_tone500_SOA200_data.pkl' 
+#filename = 'REDO_BBN_CINECA_v36_5656BF_624SOA_0_0_0_data.pkl'
+
 
 fn = basedir + filename
 
 
-
+# Load simulation data 
 sim.load(fn, instantiate=False)
 
 
-# Get stimulus times 
+# Get stimulus input times 
 stimTimes = sim.cfg.ICThalInput['startTime']
 if type(stimTimes) is not list:
 	stimTimes = [stimTimes]
 
 
 
-## CALCULATE IC SPIKE TIMES FOR BAR PLOT 
-cells_IC, cellGids_IC, netStimLabels_IC = getInclude(['IC'])	# getInclude(include)
+# ## CALCULATE IC SPIKE TIMES FOR BAR PLOT ## 
+# cells_IC, cellGids_IC, netStimLabels_IC = getInclude(['IC'])	# getInclude(include)
 
-IC_stimTimeRanges = {}
-for stimTime in stimTimes:
-	stimTimeKey = 'stim_' + str(int(stimTime)) + 'ms'
-	IC_stimTimeRanges[stimTimeKey] = [stimTime, stimTime + 50]
-
-
-spkts_IC = {}
-for i in IC_stimTimeRanges.keys():
-	timeRange = IC_stimTimeRanges[i]
-	sel, spkts, spkgids = getSpktSpkid(cellGids_IC, timeRange=timeRange) # using [] is faster for all cells
-	spkts_IC[i] = {'sel': sel, 'spkts': spkts, 'spkgids': spkgids, 'timeRange': timeRange}
-### SEL IS THE PANDAS DATA FRAME WE MIGHT WANT TO USE
-
-numStims = len(IC_stimTimeRanges.keys())
-x = list(np.arange(1,numStims+1,1))
-
-IC_spkNumsList = []
-for q in spkts_IC.keys():
-	IC_spkNumsList.append(len(spkts_IC[q]['spkts']))
+# IC_stimTimeRanges = {}
+# for stimTime in stimTimes:
+# 	stimTimeKey = 'stim_' + str(int(stimTime)) + 'ms'
+# 	IC_stimTimeRanges[stimTimeKey] = [stimTime, stimTime + 50]
 
 
+# spkts_IC = {}
+# for i in IC_stimTimeRanges.keys():
+# 	timeRange = IC_stimTimeRanges[i]
+# 	sel, spkts, spkgids = getSpktSpkid(cellGids_IC, timeRange=timeRange) # using [] is faster for all cells
+# 	spkts_IC[i] = {'sel': sel, 'spkts': spkts, 'spkgids': spkgids, 'timeRange': timeRange}
+# ### SEL IS THE PANDAS DATA FRAME WE MIGHT WANT TO USE
 
-# ## PLOT IC SPIKE BAR BLOT ON BOTTOM SUBPLOT 
+# numStims = len(IC_stimTimeRanges.keys())
+# x = list(np.arange(1,numStims+1,1))
+
+# IC_spkNumsList = []
+# for q in spkts_IC.keys():
+# 	IC_spkNumsList.append(len(spkts_IC[q]['spkts']))
+
+
+
+# ## PLOT IC SPIKE BAR BLOT ON BOTTOM SUBPLOT ## 
 # fig, (ax1,ax2) = plt.subplots(nrows=2, ncols=1, sharex=False, sharey=False)
 # ax2.bar(x, IC_spkNumsList)
 # #ax2.title('IC spikes')
-# #plt.bar(x, IC_spkNumsList) #, label=stimTimes)
-# # --> NEXT GET TITLE, LABELS ALL SORTED OUT! 
+# #plt.bar(x, IC_spkNumsList) #, label=stimTimes)			# # --> NEXT GET TITLE, LABELS ALL SORTED OUT! 
+# plt.show()
 
 
-#plt.show()
+##
+# include=['CT6']
+# cells, cellGids, netStimLabels = getInclude(include)
+# sel, spkts, spkgids = getSpktSpkid(cellGids, timeRange=timeRange) # using [] is faster for all cells
+# sel, spkts, spkgids = getSpktSpkid(cellGids=[] if include == ['allCells'] else cellGids, timeRange=timeRange) # using [] is faster for all cells
+###########
 
 
-
-## PLOT OTHER POP SPIKE TOTALS ON TOP 
-ECorticalPops = []
+## LISTS OF POPS ## 
 EThalPops = ['TC', 'TCM']
 Epops = ['IT2', 'IT3', 'ITP4', 'ITS4', 'IT5A', 'CT5A', 'IT5B', 'CT5B' , 'PT5B', 'IT6', 'CT6']  # all layers
 
-include=['CT6']#, 'IT6']#Epops   #['IT2', 'IT3', '']
-cells, cellGids, netStimLabels = getInclude(include)
-# ## 
-# # timeRange = [2500, 2550]#[1000,10000]
-# # timeRange = [2450, 2500]
-# timeRange = [5000, 5100]
-# #timeRange = [4090, 5000]
-# sel, spkts, spkgids = getSpktSpkid(cellGids, timeRange=timeRange) # using [] is faster for all cells
 
-# # sel, spkts, spkgids = getSpktSpkid(cellGids=[] if include == ['allCells'] else cellGids, timeRange=timeRange) # using [] is faster for all cells
-
-# timeRange1 = [4900, 5000]
-# sel1, spkts1, spkgids1 = getSpktSpkid(cellGids, timeRange=timeRange1)
-###########
-
-includePops = Epops #['IT6']#, 'CT6']#['CT6', 'IT6']#['CT5B', 'PT5B', 'CT6', 'IT6'] #+ ['IC'] # ['IC','CT6','IT6']
+##  Designate which POPS to include & the timeRange to plot  ## 
+includePops = Epops 	#['IT6']	#['CT6']
 timeRange = [stimTimes[0]-50, stimTimes[-1]+50]
 
-## PLOT HISTOGRAM DIRECTLY
-# sim.plotting.plotSpikeHist(include=includePops, timeRange = timeRange, binSize=5, showFig=0, stacked=False, legend='labels')
-# plt.vlines(stimTimes, 50, 250, linestyles='dashed', colors='blue')
-#plt.show()
 
-
-# # PLOT HISTOGRAM USING HISTDATA
+## PLOT HISTOGRAM USING HISTDATA ##
 histData = sim.analysis.prepareSpikeHist(
-	timeRange=timeRange, include=includePops)#Epops)#['CT6', 'IT6', 'CT5B', 'IT5B'])#binSize)
+	timeRange=timeRange, include=includePops)	# Epops		# ['CT6', 'IT6', 'CT5B', 'IT5B']
 
-rcParams = {'xtick.color': 'blue'}#, 'axes.facecolor': 'black'}
-sim.plotting.plotSpikeHist(histData=histData, include='allCells',binSize=20, showFig=0, rcParams=rcParams, legend=True, allCellsColor='darkblue', density=False,histType='stepfilled')#, density=True)#xlabel='TIME (ms)')
-
+sim.plotting.plotSpikeHist(histData=histData, include='allCells',binSize=20, showFig=0, legend=True, allCellsColor='darkblue', density=False,histType='stepfilled')		# #rcParams = {'xtick.color': 'blue'}#, 'axes.facecolor': 'black'} # rcParams=rcParams # density=True	# xlabel='TIME (ms)'
 
 plt.vlines(stimTimes, 0, 50, linestyles='dashed', colors='yellow')  # 0, 0.000300, 
 
 plt.show()
 
 
+## OR PLOT HISTOGRAM DIRECTLY ## 
+# sim.plotting.plotSpikeHist(include=includePops, timeRange = timeRange, binSize=5, showFig=0, stacked=False, legend='labels')
+# plt.vlines(stimTimes, 50, 250, linestyles='dashed', colors='blue')
+# plt.show()
+
+
+
+########## ATTEMPT TO USE PD / SEABORN TO GET ENVELOPE OF HISTOGRAM ############
 
 # binSize=50
 # spkTimes = histData['spkTimes']
@@ -127,7 +120,7 @@ plt.show()
 # sns.kdeplot(df)
 
 
-#histoBins = pd.Series(histoData[1])
+# histoBins = pd.Series(histoData[1])
 # histoCount = pd.Series(histoData[0])
 # v3 = np.concatenate((histoBins,histoCount))
 # sns.kdeplot(v3)
